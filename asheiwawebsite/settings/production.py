d0ad43db5897cce91df1d0e403e3cd01b9de2252
asheiwa <asheiwa@yahoo.com>
2017-10-20 09:31:00 +0700

    change media settings and urls

--- asheiwawebsite/settings/production.py
+++ asheiwawebsite/settings/production.py
@@ -162,11 +162,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 STATICFILES_DIRS = [
                 os.path.join(BASE_DIR, "static"),
             ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+            
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join( os.path.dirname(BASE_DIR), "media_cdn")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
 
--- asheiwawebsite/urls.py
+++ asheiwawebsite/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
 	url(r'^contact/$', views.contact),
 	url(r'^admin/', admin.site.urls),
 	url(r'^posts/', include('posts.urls', namespace='posts')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
